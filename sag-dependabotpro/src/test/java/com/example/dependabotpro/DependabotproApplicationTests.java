4badf0d28f0e6958d7243d073f35dffbcd49e898
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-11-10 15:11:04 +0700

    add dp

---
+++ sag-dependabotpro/src/test/java/com/example/dependabotpro/DependabotproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependabotpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotproApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
