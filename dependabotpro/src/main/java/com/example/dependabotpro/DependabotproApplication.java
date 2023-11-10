44c6f8c1ed69010ce332165670af834916d03147
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-11-10 13:06:08 +0700

    init pt

---
+++ dependabotpro/src/main/java/com/example/dependabotpro/DependabotproApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependabotpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotproApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DependabotproApplication.class, args);
+  }
+
+}
